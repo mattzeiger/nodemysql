ed62948b804e16aa4c38b686f76c024ac1e1efc7
mattzeiger <matt.zeiger@gmail.com>
2015-02-01 19:26:29 -0800

    Sunday commit

--- routes/customers.js
+++ routes/customers.js
@@ -8,22 +8,51 @@ router.get('/', function(req, res, next) {
 	            if(err) {
 	                console.log("Error Selecting : %s ",err );
 				}
-
-	            res.render('customers',{page_title:"Customers - Node.js",data:rows,testing:'heyos'});
+	            res.render('customers',{page_title:"Customers - Node.js",data:rows, status:req.query.status});
 
 	         });
 	    });	
 });
 
-router.all('/add', function(req, res, next) {
-	if (req.method == "POST") {
+router.all('/add', function(req, res, next) {	
+	if (req.method == "POST") {		
+		
 		var input = JSON.parse(JSON.stringify(req.body));
-		if (input.inputName) {
-			console.log(input.inputName);
+		
+		if (input.inputName) { //validate input						
+			var data = {
+				name: input.inputName,
+				address: input.inputAddress,
+				email: input.inputEmail,
+				phone: input.inputPhone
+			};
+			
+			if (input.inputID > 0) {
+				//its an update
+				console.log("Updating customer");
+			} else {			
+				//create a new customer record
+				console.log("Creating a new customer");
+				
+				
+				req.getConnection(function(err,connection){			
+					
+					var query = connection.query('INSERT INTO customer set ?', data, function(err,rows) {
+						
+						if (err) {
+							console.log("Error Selecting : %s ",err );
+						}
+						
+						res.redirect("/customers?status=created");												
+					});
+				});
+				/**/
+			}
 		}
+		//res.redirect("/customers?success=1");	
+	} else {
+		res.render('customers_edit');
 	}
-	
-	res.render('customers_edit', {hold:input});
 	
 });
 
